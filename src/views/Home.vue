07e571be36d582b6cccfe692236ad70a04dfcd45
lijialin <lijialin@mayi888.com>
2020-02-13 17:40:43 +0800

    元素随鼠标移动

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,98 +1,67 @@
 <template>
-  <div class="player-bottom-panel">
-    <!-- 要生成海报的-->
-    <div class="creat-give-poster" ref="creatGivePoster">
-      <p class="player-bottom-panel-title">赠送成功</p>
-      <p class="player-bottom-panel-tip center">快扫码支持</p>
-      <vue-qr
-        :logo-src="config.logo"
-        :text="config.value"
-        :size="100"
-        :margin="0"
-        class="player-bottom-panel-qrcode center"
-      />
+  <div class="home">
+    <div class="mousePosition">
+      <p>鼠标位置:{{mouseP.mouseX}}X,{{mouseP.mouseY}}Y</p>
     </div>
-
-    <!-- 生成海报的-->
-    <div class="share-give-poster">
-      <img class="poster-img" :src="imgUrl" />
+    <div class="content">
+      <img class="img1" ref="img1" src="https://c-ssl.duitang.com/uploads/item/201509/30/20150930140213_ZCFSr.jpeg" alt="">
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import VueQr from "vue-qr";
-import html2canvas from "html2canvas";
 export default {
-  name: "home",
+  name: "",
   data() {
     return {
-      //二维码参数
-      config: {
-        value: '123', //显示的值、跳转的地址
-        // logo: "www.duitang.com/blog/?id=747022744" //中间logo的地址
+      // 鼠标位置
+      mouseP: {
+        mouseX: 0,
+        mouseY: 0
       },
-      imgUrl: "" //最后转化出来的图片base64地址
+      // 鼠标移动属性
+      moveObj: {}
     };
   },
-  components: {
-    VueQr
-  },
+  components: {},
   computed: {},
   created() {},
   mounted() {
-    setTimeout(_ => {
-      this.getPoster();
-    }, 500);
+    this.init();
   },
   methods: {
-    // 生成海报
-    getPoster() {
-      let shareContent = this.$refs.creatGivePoster; //需要截图的包裹的（原生的）DOM 对象
-      let width = shareContent.offsetWidth; //获取dom 宽度
-      let height = shareContent.offsetHeight; //获取dom 高度
-      let canvas = document.createElement("canvas"); //创建一个canvas节点
-      let scale = 1; //定义任意放大倍数 支持小数
-      canvas.width = width * scale; //定义canvas 宽度 * 缩放
-      canvas.height = height * scale; //定义canvas高度 *缩放
-      canvas.getContext("2d").scale(scale, scale); //获取context,设置scale
-      let opts = {
-        scale: scale, // 添加的scale 参数
-        canvas: canvas, //自定义 canvas
-        // logging: true, //日志开关，便于查看html2canvas的内部执行流程
-        width: width, //dom 原始宽度
-        height: height,
-        useCORS: true, // 【重要】开启跨域配置
-        backgroundColor: null
+    init() {
+      window.onmousemove = e => {
+        console.log(e);
+        this.moveObj = e;
+        this.mouseP = {
+          mouseX: e.x,
+          mouseY: e.y
+        };
+        this.elementMove();
       };
-
-      html2canvas(shareContent, opts).then(canvas => {
-        var context = canvas.getContext("2d");
-        // 【重要】关闭抗锯齿
-        context.mozImageSmoothingEnabled = false;
-        context.webkitImageSmoothingEnabled = false;
-        context.msImageSmoothingEnabled = false;
-        context.imageSmoothingEnabled = false;
-
-        // 【重要】默认转化的格式为png,也可设置为其他格式
-        let imgUrl = canvas.toDataURL("image/png");
-        this.imgUrl = imgUrl;
-        console.log("生成海报");
-      });
-    }
+    },
+    elementMove(){
+      this.$refs.img1.style.left=this.mouseP.mouseX+'px';
+      this.$refs.img1.style.top=this.mouseP.mouseY+'px';
+    },
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.creat-give-poster{
-  width: 200px;
-  height: 200px;
-}
-.share-give-poster{
-  width: 200px;
-  img{
-    width: 100%;
+.content{
+  width: 400px;
+  height: 400px;
+  position: relative;
+  border: 1px solid #000;
+  margin: 0 auto;
+  .img1{
+    top: 0;
+    left: 0;
+    position: fixed;
+    width: 100px;
+    height: 100px;
   }
 }
 </style>
